ee17ab732b61326252cd1c16107794c9fdd2ad54
Ricardo Pesce <rspesce@gmail.com>
2021-07-09 02:40:47 -0300

    funcionalidades da janela de edição/exclusão de registro

--- editar.cpp
+++ editar.cpp
@@ -1,14 +1,50 @@
 #include "editar.h"
 #include "ui_editar.h"
 
-Editar::Editar(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::Editar)
-{
+Editar::Editar(QWidget *parent, int id, QString todo) : QDialog(parent), ui(new Ui::Editar){
     ui->setupUi(this);
+    ui->label_id->setText( QString::number(id) );
+    ui->field_txt->setText( todo );
 }
 
 Editar::~Editar()
 {
     delete ui;
+}
+
+void Editar::on_btn_edit_clicked(){
+
+    QString str_todo = ui->field_txt->text();
+    QString id = ui->label_id->text();
+
+    if(str_todo == ""){
+        QMessageBox::warning(this, "AVISO", "Campo não pode estar vazio");
+        return;
+    }
+
+    QSqlQuery query;
+    QString sql = "UPDATE todos SET todos='" + str_todo + "' WHERE id=" + id;
+    query.prepare( sql );
+
+    if( query.exec() ){
+        close();
+    } else{
+        qDebug() << "Falha ao atualizar registro";
+    }
+}
+
+void Editar::on_btn_excluir_clicked(){
+
+    //QMessageBox::warning(this, "ATENÇÃO!", "Você tem certeza que deseja excluir este registro permanentemente?");
+
+    QString id = ui->label_id->text();
+    QSqlQuery query;
+    QString sql = "DELETE FROM todos WHERE id=" + id;
+    query.prepare( sql );
+
+    if( query.exec() ){
+        close();
+    } else{
+        qDebug() << "Falha ao deletar registro";
+    }
 }
--- editar.h
+++ editar.h
@@ -2,6 +2,9 @@
 #define EDITAR_H
 
 #include <QDialog>
+#include <QtSql>
+#include <QDebug>
+#include <QMessageBox>
 
 namespace Ui {
 class Editar;
@@ -12,8 +15,13 @@ class Editar : public QDialog
     Q_OBJECT
 
 public:
-    explicit Editar(QWidget *parent = nullptr);
+    explicit Editar(QWidget *parent = nullptr, int id = 0, QString todo = NULL);
     ~Editar();
+
+private slots:
+    void on_btn_edit_clicked();
+
+    void on_btn_excluir_clicked();
 
 private:
     Ui::Editar *ui;
